9419966a0eb6a4f67fc2e5dc2a9a9341b6c2305f
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-21 23:58:22 -0500

    add show_menu() method

--- views/student_view.py
+++ views/student_view.py
@@ -1,1 +1,10 @@
 class StudentView:
+
+    def show_menu(self):
+        print("\nStudent Record Manager")
+        print("1. Add Student")
+        print("2. View Students")
+        print("3. Update Student")
+        print("4. Delete Student")
+        print("5. Show Statistics")
+        print("6. Exit")
