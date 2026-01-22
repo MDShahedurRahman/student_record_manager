c87c7bf0f5ff8190747ad6e019a6c8ef8b0cc778
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-21 23:51:32 -0500

    add main() function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from controllers.student_controller import StudentController
+
+
+def main():
+    controller = StudentController()
+    controller.run()
+
+
+if __name__ == "__main__":
+    main()
