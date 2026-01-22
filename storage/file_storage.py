39ec11670360ced3011836762d5232304b26ce49
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-22 00:04:05 -0500

    implement load_data() function

--- storage/file_storage.py
+++ storage/file_storage.py
@@ -2,3 +2,10 @@ import json
 import os
 
 FILE_NAME = "students.json"
+
+
+def load_data():
+    if not os.path.exists(FILE_NAME):
+        return []
+    with open(FILE_NAME, "r") as f:
+        return json.load(f)
