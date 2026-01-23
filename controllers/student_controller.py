a8d13ac906d0a954a30ae8510c97e0c505bd1fd3
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-22 23:35:21 -0500

    create StudentController class

---
+++ controllers/student_controller.py
@@ -0,0 +1,7 @@
+from views.student_view import StudentView
+from services.student_service import StudentService
+from models.student import Student
+from utils.validators import is_valid_age, is_valid_grade
+
+
+class StudentController:
